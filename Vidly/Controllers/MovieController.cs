fc5489c239d896bcb6c08ce705b5358f0cce0145
iswebgig <brandongalli32@gmail.com>
2022-11-18 09:43:04 -0500

    init

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Brandon" };
+
+            //return View(movie);
+            //return Content("Hello world");
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id); 
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}, sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
